5c73a0a23902b70d38f11f6e93dcc75f7aef2b2a
chengmiao <673094766@qq.com>
2019-02-20 13:52:40 +0800

    11

--- PBtest.lua
+++ PBtest.lua
@@ -19,28 +19,11 @@ assert(protoc:load [[
       repeated Phone  contacts = 4;
    } ]])
 
--- lua table data
-local data = {
-   name = "ilse",
-   age  = 18,
-   contacts = {
-      { name = "alice", phonenumber = 12312341234 },
-      { name = "bob",   phonenumber = 45645674567 }
-   }
-}
-
-for name in pb.fields(".Person") do
-  print(name)
-  print(pb.field(".Person", name))
-  print(pb.type(name))
-end
-
---print(pb.type(".Person"))
-
+local data = testdata
 -- encode lua table data into binary format in lua string and return
---local bytes = assert(pb.encode("Person", data))
---print(pb.tohex(testdata))
+local bytes = assert(pb.encode("Person", data))
+print(pb.tohex(testdata))
 
 -- and decode the binary data back into lua table
---local data2 = assert(pb.decode("Person", testdata))
---print(require "serpent".block(data2))
+local data2 = assert(pb.decode("Person", bytes))
+print(require "serpent".block(data2))
